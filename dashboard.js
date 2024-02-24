73a54cec1a6e959c8dfa68d902041c86adf8331f
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-02-24 11:48:04 +0530

    html,css and js added

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,24 @@
 console.log("dashboard.js");
 
+let answeredQuestions = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+];
+
 let profile = document.querySelector(".profile");
 
 profile.addEventListener("click", ()=>{
@@ -14,4 +33,17 @@ side_button.addEventListener("click",()=>{
     side_button.classList.toggle("active");
     side_bar.classList.toggle("active");
     main.classList.toggle("active");
-})
+});
+
+let question = document.querySelector(".question");
+let question_options = document.querySelector(".question_options");
+
+
+function checkAllAnswered(){
+    let countOfFalse = answeredQuestions.filter(value => value === true).length;
+    if(countOfFalse == 16){
+        alert("congratulations");
+    }
+}
+
+checkAllAnswered();
